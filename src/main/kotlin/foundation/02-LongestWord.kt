50d18ad7f6eadeacc2566e8a6e425e3e23ce39e8
PETERSBURG\Leonid_Borisevich <leonid_borisevich@epam.com>
2018-04-28 20:39:21 +0300

    working on optimal longest word

--- src/main/kotlin/foundation/02-LongestWord.kt
+++ src/main/kotlin/foundation/02-LongestWord.kt
@@ -13,7 +13,37 @@ fun longestWord(string: String, dictionary: Set<String>): String {
             .orEmpty()
 }
 
+fun optimalLongestWord(string: String, dictionary: Set<String>): String {
+    val map = dictionary
+            .map { Tuple(it, 0) }
+            .groupingBy { it.word[it.index] }
+            .foldTo(mutableMapOf<Char, MutableSet<Tuple>>(), mutableSetOf()) { accumulator, element ->
+                accumulator.add(element)
+                accumulator
+            }
+    string.forEach { c ->
+        map[c]?.let { set ->
+            val updatedMap = mutableMapOf<Char, MutableSet<Tuple>>()
+            set.forEach { tuple ->
+                tuple.index += 1
+                updatedMap[c]?.add(tuple) ?: updatedMap.put(c, mutableSetOf(tuple))
+            }
+            updatedMap.forEach { c, updatedSet ->
+                val existingList = map[c]
+                if (existingList != null) existingList.addAll(updatedSet) else map[c] = updatedSet
+            }
+        }
+    }
+    return map.flatMap { it.value }
+            .filter { it.index == string.length }
+            .map { it.word }
+            .firstOrNull() ?: ""
+}
+
 
 fun main(args: Array<String>) {
     print(longestWord("abppplee", setOf("able", "ale", "apple", "bale", "kangaroo")))
+    print(optimalLongestWord("abppplee", setOf("able", "ale", "apple", "bale", "kangaroo")))
 }
+
+data class Tuple(val word: String, var index: Int)
